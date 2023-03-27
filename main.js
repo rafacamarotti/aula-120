e98a4a625a704549425344a5a77039b10dc267de
rafacamarotti <110203753+rafacamarotti@users.noreply.github.com>
2023-03-27 20:08:29 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
-    classifier= ml5.imageClassifier('Mobilnet', modelloaded);
+    classifier= ml5.imageClassifier('MobileNet', modelloaded);
 }
 function modelloaded(){
-    console.lod('modelo carregado');
+    console.log('modelo carregado');
 }
 function draw(){
     image(video, 0, 0, 300, 300);
@@ -20,10 +20,12 @@ function gotResult(error, results){
     if((results[0].confidence > 0.5) && (previousResult != results[0].label)){
         console.log(results);
         previousResult= results[0].label;
-        synth = window.SpeechSynthesisUtterance(speakData);
+        synth = window.speechSynthesis;
+        speakData= 'o objeto detectado é - '+results[0].label;
+        utterThis= new SpeechSynthesisUtterance(speakData);
         synth.speak(utterThis);
         document.getElementById("resultObjectName").innerHTML = results[0].label;
-        document.getElementById("resultObjectAccuarcy").innerHTML = results[0].confidence.toFixed(3);
+        document.getElementById("resultObjectAccuracy").innerHTML = results[0].confidence.toFixed(3);
     }
   }
 }
